f123d7207df00a3ba6f8359e147e6fe379bffb61
stoneyboi <stonej2@mail.gvsu.edu>
2021-12-07 22:58:03 -0600

    up to recent workouts tab

---
+++ iRow/LogInViewController.swift
@@ -0,0 +1,33 @@
+//
+//  LogInViewController.swift
+//  iRow
+//
+//  Created by Jake Stone on 12/7/21.
+//
+
+import UIKit
+
+class LogInViewController: UIViewController {
+    @IBOutlet weak var EmailField: UITextField!
+    @IBOutlet weak var PassField: UITextField!
+    @IBOutlet weak var LogInButton: UIButton!
+    @IBOutlet weak var SignUpButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
